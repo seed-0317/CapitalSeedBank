963e9c255a9280c43e5a405d830409f4ebf1718e
Mustanski <nof191@f45c89aa4f4d.cof.ds.capitalone.com>
2017-04-10 13:39:00 -0400

    add menu selection

--- src/main/java/com/CapitalSeedBank/Main/Application.java
+++ src/main/java/com/CapitalSeedBank/Main/Application.java
@@ -14,58 +14,62 @@ public class Application {
     public static void main(String[] args) {
 
 
-        //Account newAcct = new Account(1, 100);
 
         AccountDAO dao = new AccountDAOImpl();
 
-        // test make an account
-        // dao.makeAccount(newAcct);
+        System.out.println("What would you like to do? ");
+        System.out.println("1 - Open new account with $100 balance ");
+        System.out.println("2 - Check balance ");
+        System.out.println("3 - Make a deposit ");
+        System.out.println("4 - Withdraw cash");
 
-        // test get an account
-        // Account temp = dao.getAccount();
-
-        //System.out.println(temp);
-
-        /* code to make a deposit
-
-        Account temp = dao.getAccount();
-
-        //later we can prompt the user for deposit amount
-
-        System.out.println("Enter your deposit amount: ");
+        System.out.println("Enter choice: ");
         Scanner scanner = new Scanner(System.in);
-        int depAmount = Integer.parseInt(scanner.nextLine());
+        int ch = Integer.parseInt(scanner.nextLine());
+
+        if (ch ==1) {
+            // test make an account
+            Account newAcct = new Account(1, 100);
+            dao.makeAccount(newAcct);
+        } else
+            if (ch ==2) {
+                 Account temp = dao.getAccount();
+                 System.out.println(temp);
+            }else
+                if (ch == 3) {
+            //code to make a deposit
+
+                Account temp = dao.getAccount();
 
 
+                System.out.println("Enter your deposit amount: ");
+                    scanner = new Scanner(System.in);
+                int depAmount = Integer.parseInt(scanner.nextLine());
 
-        //int depAmount = 100;
+                temp.setBalance(temp.getBalance()+Math.abs(depAmount));
 
-        temp.setBalance(temp.getBalance()+Math.abs(depAmount));
+                dao.makeAccount(temp);
+                } else
+                    if (ch == 4) {
+                        Account temp = dao.getAccount();
 
-        dao.makeAccount(temp);
+                        System.out.println("Enter your withdraw amount: ");
+                        scanner = new Scanner(System.in);
+                        int withAmount = Integer.parseInt(scanner.nextLine());
 
-        */
+                        //int withdrawlAmount = 100;
 
-        // code to make a withdraw
+                        if (temp.getBalance() - Math.abs(withAmount) < 0) {
+                            System.out.println("Withdraw will make balance negative");
+                        } else {
 
-        Account temp = dao.getAccount();
+                            int tempBal = temp.getBalance();
+                            tempBal = tempBal - Math.abs(withAmount);
+                            temp.setBalance(tempBal);
+                            dao.makeAccount(temp);
+                        }
 
-        System.out.println("Enter your withdraw amount: ");
-        Scanner scanner = new Scanner(System.in);
-        int withAmount = Integer.parseInt(scanner.nextLine());
-
-        //int withdrawlAmount = 100;
-
-        if (temp.getBalance() - Math.abs(withAmount) < 0) {
-            System.out.println("Withdraw will make balance negative");
-        } else {
-
-            int tempBal = temp.getBalance();
-            tempBal = tempBal - Math.abs(withAmount);
-            temp.setBalance(tempBal);
-            dao.makeAccount(temp);
-        }
-
+                    }
 
     }
 
